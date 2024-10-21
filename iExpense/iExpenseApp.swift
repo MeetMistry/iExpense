ae52cb7c28c005e24ea036865c94cac3ac48b739
Meet Mistry <meet.m@simformsolutions.com>
2024-10-21 07:15:47 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Meet Mistry on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
